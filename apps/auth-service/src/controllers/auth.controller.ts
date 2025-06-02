505f032b3488e1889a51cfd9f5c421e55c4dce5d
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-06-03 01:55:24 +0530

    login api created

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,9 +1,11 @@
 import {Request,Response,NextFunction} from "express";
 import { checkOtpRestrictions, sendOtp, trackOtpRequests, validateRegistrationData, verifyOtp } from "../utils/auth.helper";
 import prisma from "@packages/libs/prisma";
-import { ValidationError } from "@packages/error-handlers";
+import { AuthError, ValidationError } from "@packages/error-handlers";
 import bcrypt from "bcryptjs";
 import {StatusCodes} from "http-status-codes";
+import jwt from "jsonwebtoken";
+import { setCookie } from "../utils/cookies/setCookie";
 
 // Reggister a new user
 
@@ -65,5 +67,47 @@ export const verifyUser = async(req:Request,res:Response,next:NextFunction) => {
 
 /** Login users */
 export const loginUser = async(req:Request,res:Response,next:NextFunction) =>{
+  try{
+    const {email, password} = req.body;
 
+    if(!email || !password){
+      throw new ValidationError("Email and password are required");
+    }
+
+    const user = await prisma.users.findUnique({where: {email}});
+
+    if(!user){
+      throw new AuthError("User doesn't exists!");
+    }
+
+    // verify password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+      return new AuthError("Invalid email or password");
+    }
+
+    // Generate access token and refresh token
+    const accessToken = jwt.sign({id:user.id, role:"user"},process.env.JWT_ACCESS_SECRET as string,{
+      expiresIn: "15m"
+    });
+    const refreshToken = jwt.sign({id:user.id, role:"user"},process.env.JWT_REFRESH_SECRET as string,{
+      expiresIn: "7d"
+    });
+
+    // store the refresh and access token in an httpOnly secure cookie
+    setCookie(res,"access_token",accessToken);
+    setCookie(res,"refresh_token",refreshToken);
+
+    res.status(StatusCodes.OK).json({
+      message: "Login Successfully",
+      user:{
+        id: user.id,
+        email: user.email,
+        name: user.name
+      }
+    })
+
+  }catch(error){
+    return next(error);
+  }
 }
