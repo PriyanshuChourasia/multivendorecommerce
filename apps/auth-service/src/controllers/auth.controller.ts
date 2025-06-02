797b9415c9f8cb721015efe2d660f698c4e6b328
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-06-02 12:21:02 +0530

    controller added in auth-service sendEmail helper setup done

---
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -0,0 +1,20 @@
+import {Request,Response,NextFunction} from "express";
+import { checkOtpRestrictions, validateRegistrationData } from "../utils/auth.helper";
+import prisma from "../../../../packages/libs/prisma";
+import { ValidationError } from "../../../../packages/error-handlers";
+
+// Reggister a new user
+
+export const userRegistration = async (req:Request,res:Response,next:NextFunction) =>{
+  validateRegistrationData(req.body,"user");
+  const {name,email} = req.body;
+
+  const existingUser = await prisma.users.findUnique({where:email});
+
+  if(existingUser){
+    return next(new ValidationError("User already exists with this email"));
+  }
+
+  await checkOtpRestrictions(email,next);
+
+}
